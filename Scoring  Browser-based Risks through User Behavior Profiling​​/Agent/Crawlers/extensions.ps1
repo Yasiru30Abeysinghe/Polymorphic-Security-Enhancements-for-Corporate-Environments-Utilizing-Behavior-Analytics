8cade7efca9cb7a922b3ccbdfed6315a44d859bd
Yasiru Pathmila Perera Abeysinghe <it21022142@my.sliit.lk>
2024-09-09 21:31:16 +0530

    Upload New File

---
+++ Scoring  Browser-based Risks through User Behavior Profiling​​/Agent/Crawlers/extensions.ps1
@@ -0,0 +1,36 @@
+﻿# Function to get the extension directories for Chrome
+function Get-ChromeExtensions {
+    $chromeExtensionsPath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Extensions"
+    
+    if (Test-Path $chromeExtensionsPath) {
+        return Get-ChildItem -Path $chromeExtensionsPath | Where-Object { $_.PSIsContainer }
+    } else {
+        Write-Output "Chrome extensions directory not found."
+        return @()
+    }
+}
+
+# Function to get the extension directories for Edge
+function Get-EdgeExtensions {
+    $edgeExtensionsPath = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Extensions"
+    
+    if (Test-Path $edgeExtensionsPath) {
+        return Get-ChildItem -Path $edgeExtensionsPath | Where-Object { $_.PSIsContainer }
+    } else {
+        Write-Output "Edge extensions directory not found."
+        return @()
+    }
+}
+
+# Get Chrome and Edge extension IDs
+$chromeExtensions = Get-ChromeExtensions
+$edgeExtensions = Get-EdgeExtensions
+
+Write-Output "Combined Extension IDs for Chrome and Edge:"
+
+# Combine the extension IDs from both browsers
+$allExtensions = $chromeExtensions + $edgeExtensions
+
+foreach ($extension in $allExtensions) {
+    Write-Output $extension.Name
+}
