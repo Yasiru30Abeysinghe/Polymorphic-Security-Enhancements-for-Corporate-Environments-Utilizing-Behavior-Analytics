43e21d0050c5a855f7ed1b28b8fa8398cf0b2c37
Yasiru Pathmila Perera Abeysinghe <it21022142@my.sliit.lk>
2024-09-09 21:32:23 +0530

    Upload New File

---
+++ Scoring  Browser-based Risks through User Behavior Profiling​​/Agent/Crawlers/chromeversion.ps1
@@ -0,0 +1,41 @@
+﻿# Function to find the Edge executable
+function Find-EdgeExecutable {
+    $paths = @(
+        "$env:PROGRAMFILES\Microsoft\Edge\Application\msedge.exe",
+        "$env:PROGRAMFILES(X86)\Microsoft\Edge\Application\msedge.exe",
+        "$env:LOCALAPPDATA\Microsoft\Edge\Application\msedge.exe",
+        "$env:PROGRAMFILES\Microsoft\Edge Beta\Application\msedge.exe",
+        "$env:PROGRAMFILES(X86)\Microsoft\Edge Beta\Application\msedge.exe",
+        "$env:PROGRAMFILES\Microsoft\Edge Dev\Application\msedge.exe",
+        "$env:PROGRAMFILES(X86)\Microsoft\Edge Dev\Application\msedge.exe"
+    )
+
+    foreach ($path in $paths) {
+        if (Test-Path $path) {
+            return $path
+        }
+    }
+
+    Write-Output "Edge executable not found."
+    exit
+}
+
+# Function to get the Edge version
+function Get-EdgeVersion {
+    param (
+        $edgeExecutable
+    )
+
+    # Get the version information from the executable
+    $versionInfo = (Get-Item $edgeExecutable).VersionInfo
+    return $versionInfo.ProductVersion
+}
+
+# Find the Edge executable
+$edgeExecutable = Find-EdgeExecutable
+
+# Get the Edge version
+$edgeVersion = Get-EdgeVersion -edgeExecutable $edgeExecutable
+
+# Output the Edge version
+Write-Output "Microsoft Edge version: $edgeVersion"
